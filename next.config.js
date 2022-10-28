c4aa3cb34dbe1df44d3ae094b6e163e64fda5de4
isuru-dilshan <85498667+isuru-dilshan@users.noreply.github.com>
2022-10-28 19:23:35 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      'th.bing.com',
+      'lh3.googleusercontent.com'
+  ],
+  }
 }
 
 module.exports = nextConfig
